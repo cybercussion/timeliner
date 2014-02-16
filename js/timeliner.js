e29ac8e5bf27dd9e80f7bd36eb02fd271dacb4a6
Mark Statkus <mark@cybercussion.com>
2014-02-16 15:30:05 -0800

    Made several optimizations to reduce repeated JQuery selector statements, slower $.each and loop statements.  Reformatted code to get closer to JSLint friendly formatting.

--- js/timeliner.js
+++ js/timeliner.js
@@ -1,155 +1,127 @@
+/*global document*/
 /*
-* Timeliner.js
-* @version		1.6
-* @copyright	Tarek Anandan (http://www.technotarek.com)
-*/
-;(function($) {
+ * Timeliner.js
+ * @version 1.6
+ * @copyright Tarek Anandan (http://www.technotarek.com)
+ * @modified By Mark Statkus to meet formatting specs and optimizations (less retyping)
+ */
+(function ($) {
+    "use strict";
+    /**
+     * Start Timeliner
+     * @param options {object}
+     */
+    function startTimeliner(options) {
+        // Mark: I fixed this.  It's self documented code options and can be extended easily.
+        var defaults = {
+                timelineContainer: '#timelineContainer',
+                startState       : 'closed',
+                startOpen        :
+                    [
+                    ],
+                baseSpeed        : 200,
+                speed            : 4,
+                fontOpen         : '1.2em',
+                fontClosed       : '1em',
+                expandAllText    : '+ expand all',
+                collapseAllText  : '- collapse all'
+            }, settings = $.extend(defaults, options), // popular way to extend a existing set of data
+            key, p, $value, $tlEvent = $(settings.timelineContainer + " " + ".timelineEvent");
 
-        $.timeliner = function(options) {
-            if ($.timeliners == null) {
-                $.timeliners = { options: [] };
-                $.timeliners.options.push(options);
-            }
-            else {
-                $.timeliners.options.push(options);
-            }
-            $(document).ready(function() {
-                for (var i=0; i<$.timeliners.options.length; i++) {
-                    startTimeliner($.timeliners.options[i]);
-                }
-            });
+        function openEvent(eventHeading, eventBody) {
+            $(eventHeading).removeClass('closed').addClass('open').animate({ fontSize: settings.fontOpen }, settings.baseSpeed);
+            $(eventBody).show(settings.speed * settings.baseSpeed);
         }
 
-        function startTimeliner(options){
-            var settings = {
-                timelineContainer: options['timelineContainer'] || '#timelineContainer', // value: selector of the main element holding the timeline's content, default to #timelineContainer
-                startState: options['closed'] || 'closed', // value: closed | open,
-                // default to closed; sets whether the timeline is
-                // initially collapsed or fully expanded
-                startOpen: options['startOpen'] || [], // value: array of IDs of
-                // single timelineEvents, default to empty; sets
-                // the minor events that you want to display open
-                // by default on page load
-                baseSpeed: options['baseSpeed'] || 200, // value: numeric, default to
-                // 200; sets the base speed for animation of the
-                // event marker
-                speed: options['speed'] || 4, // value: numeric, defalut to 4; a
-                // multiplier applied to the base speed that sets
-                // the speed at which an event's conents are
-                // displayed and hidden
-                fontOpen: options['fontOpen'] || '1.2em', // value: any valid CSS
-                // font-size value, defaults to 1em; sets the font
-                // size of an event after it is opened
-                fontClosed: options['fontClosed'] || '1em', // value: any valid CSS
-                // font-size value, defaults to 1em; sets the font
-                // size of an event after it is closed
-                expandAllText: options ['expandAllText'] || '+ expand all', // value:
-                // string, sets the text of the expandAll selector
-                // after the timeline is fully collapsed
-                collapseAllText: options['collapseAllText'] || '- collapse all' // // value:
-                // string, sets the text of the expandAll selector
-                // after the timeline is fully expanded
-            };
-
-            function openEvent(eventHeading,eventBody) {
-                $(eventHeading)
-                    .removeClass('closed')
-                    .addClass('open')
-                    .animate({ fontSize: settings.fontOpen }, settings.baseSpeed);
-                $(eventBody).show(settings.speed*settings.baseSpeed);
-            }
-
-            function closeEvent(eventHeading,eventBody) {
-                $(eventHeading)
-                    .animate({ fontSize: settings.fontClosed }, 0)
-                    .removeClass('open')
-                    .addClass('closed');
-                $(eventBody).hide(settings.speed*settings.baseSpeed);
-            }
+        function closeEvent(eventHeading, eventBody) {
+            $(eventHeading).animate({ fontSize: settings.fontClosed }, 0).removeClass('open').addClass('closed');
+            $(eventBody).hide(settings.speed * settings.baseSpeed);
+        }
 
 
-            if ($(settings.timelineContainer).data('started')) {
-                return;                 // we already initialized this timelineContainer
-            } else {
-                $(settings.timelineContainer).data('started', true);
-                $(settings.timelineContainer+" "+".expandAll").html(settings.expandAllText);
-                $(settings.timelineContainer+" "+".collapseAll").html(settings.collapseAllText);
+        if ($(settings.timelineContainer).data('started')) {
+            return; // we already initialized this timelineContainer
+        } // Mark: no need for a else
+        $(settings.timelineContainer).data('started', true);
+        $(settings.timelineContainer + " " + ".expandAll").html(settings.expandAllText);
+        $(settings.timelineContainer + " " + ".collapseAll").html(settings.collapseAllText);
 
-			// If startState option is set to closed, hide all the events; else, show fully expanded upon load
-			if(settings.startState==='closed')
-			{
-				// Close all items
-                                $(settings.timelineContainer+" "+".timelineEvent").hide();
-
-				// show startOpen events
-				$.each($(settings.startOpen), function(index, value) {
-                                    openEvent($(value).parent(settings.timelineContainer+" "+".timelineMinor").find("dt a"),$(value));
-				});
-
-			}else{
-
-				// Open all items
-                                openEvent($(settings.timelineContainer+" "+".timelineMinor dt a"),$(settings.timelineContainer+" "+".timelineEvent"));
-
-			}
-
-			// Minor Event Click
-			$(settings.timelineContainer).on("click",".timelineMinor dt",function(){
-
-				var currentId = $(this).attr('id');
-
-				// if the event is currently open
-				if($(this).find('a').is('.open'))
-				{
-
-					closeEvent($("a",this),$("#"+currentId+"EX"))
-
-				} else{ // if the event is currently closed
-
-					openEvent($("a", this),$("#"+currentId+"EX"));
-
-				}
-
-			});
-
-			// Major Marker Click
-			$(settings.timelineContainer).on("click",".timelineMajorMarker",function()
-			{
-
-				// number of minor events under this major event
-				var numEvents = $(this).parents(".timelineMajor").find(".timelineMinor").length;
-
-				// number of minor events already open
-				var numOpen = $(this).parents(".timelineMajor").find('.open').length;
-
-				if(numEvents > numOpen )
-				{
-
-					openEvent($(this).parents(".timelineMajor").find("dt a","dl.timelineMinor"),$(this).parents(".timelineMajor").find(".timelineEvent"));
-
-				} else{
-
-					closeEvent($(this).parents(".timelineMajor").find("dl.timelineMinor a"),$(this).parents(".timelineMajor").find(".timelineEvent"));
-
-				}
-			});
-
-			// All Markers/Events
-                        $(settings.timelineContainer+" "+".expandAll").click(function()
-			{
-				if($(this).hasClass('expanded'))
-				{
-
-					closeEvent($(this).parents(settings.timelineContainer).find("dt a","dl.timelineMinor"),$(this).parents(settings.timelineContainer).find(".timelineEvent"));
-					$(this).removeClass('expanded').html(settings.expandAllText);
-
-				} else{
-
-					openEvent($(this).parents(settings.timelineContainer).find("dt a","dl.timelineMinor"),$(this).parents(settings.timelineContainer).find(".timelineEvent"));
-					$(this).addClass('expanded').html(settings.collapseAllText);
-
-				}
-			});
+        // If startState option is set to closed, hide all the events; else, show fully expanded upon load
+        if (settings.startState === 'closed') {
+            // Close all items
+            $tlEvent.hide();
+            // Mark: Each statements are 91% slower (JSPerf)
+            // Mark: Multiple $(value) selectors.. rolling up.  I also see duplicate uses below.
+            // show startOpen events
+            p = settings.startOpen; // shortener
+            for (key in p) {
+                if (p.hasOwnProperty(key)) {
+                    $value = $(p[key]);
+                    openEvent($value.parent(settings.timelineContainer + ' .timelineMinor').find('dt a'), $value);
                 }
-	};
+            }
+        } else {
+            // Open all items
+            openEvent($(settings.timelineContainer + ' .timelineMinor').find('dt a'), $tlEvent);
+        }
+
+        // Minor Event Click
+        $(settings.timelineContainer).on("click", ".timelineMinor dt", function () {
+            var $this = $(this), currentId = $this.attr('id');
+            // if the event is currently open
+            if ($this.find('a').is('.open')) {
+                closeEvent($("a", this), $("#" + currentId + "EX")); // missing semicolon
+            } else { // if the event is currently closed
+                openEvent($("a", this), $("#" + currentId + "EX"));
+            }
+        });
+
+        // Major Marker Click
+        $(settings.timelineContainer).on("click", ".timelineMajorMarker", function () {
+            // Mark: Lots of retyping the same stuff.  Simplify (Less JQuery Churn).
+            var $this = $(this), $tlMajor = $this.parents(".timelineMajor"), $tlMM = $tlMajor.find("dt a", "dl.timelineMinor"), $tlEvent = $tlMajor.find(".timelineEvent"), // number of minor events under this major event
+                numEvents = $tlMajor.find(".timelineMinor").length, numOpen = $tlMajor.find('.open').length; // number of minor events already open
+            if (numEvents > numOpen) {
+                openEvent($tlMM, $tlEvent);
+            } else {
+                closeEvent($tlMM, $tlEvent);
+            }
+        });
+
+        // All Markers/Events
+        $(settings.timelineContainer + " " + ".expandAll").click(function () {
+            // Mark: Lots of retyping the same stuff.  Simplify (Less JQuery Churn).
+            var $this = $(this), $tlC = $this.parents(settings.timelineContainer), $tlMinor = $tlC.find("dt a", "dl.timelineMinor"), $tlEvent = $tlC.find(".timelineEvent");
+            if ($(this).hasClass('expanded')) {
+                closeEvent($tlMinor, $tlEvent);
+                $(this).removeClass('expanded').html(settings.expandAllText);
+            } else {
+                openEvent($tlMinor, $tlEvent);
+                $(this).addClass('expanded').html(settings.collapseAllText);
+            }
+        });
+    }
+
+    /**
+     * timeliner
+     * Main JQuery API
+     * @param options {object}
+     */
+    $.timeliner = function (options) {
+        if ($.timeliners === undefined) { // prior was sloppy
+            $.timeliners = { options:
+                [
+                ] };
+            $.timeliners.options.push(options);
+        } else {
+            $.timeliners.options.push(options);
+        }
+        $(document).ready(function () {
+            var i = 0, len = $.timeliners.options.length;
+            while (i < len) { // while is faster (JSPerf).
+                startTimeliner($.timeliners.options[i]);
+                i += 1;
+            }
+        });
+    };
 })(jQuery);
